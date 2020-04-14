57eaae28ff734d65b4284b1b0a0d9dd4225cf7c3
BAHAR ALIPANAHI <bahar.panahie@gmail.com>
2020-04-14 13:14:36 -0400

    loading graph added

--- test/runDelVerify.bash
+++ test/runDelVerify.bash
@@ -1,51 +1,14 @@
-
-dsk="../dsk-1.6906/dsk"
-CosmoPack="../cosmo-pack"
 cosmodel="../cosmo-delete-verify"
-k=64
-
-#OutFile = "./cmpResultsCosmoBowe.txt"
+k=31
 
 if [ $# -lt 2 ]; then
-    echo "Usage: $0 <fastafilebase> <fastafile2base>"
+    echo "Usage: $0 <.dbg file> <.fasta file>"
     exit
 fi
 
-fnamebase="$1"
-fileFasta="$1.fasta"
+graph="$1"
+fastaFile="$2"
 
-fnamebase2="$2"
-fileFasta2="$2.fasta"
-
-cmd="$dsk $fileFasta $k"
+cmd="$cosmodel $graph $fastaFile"
 echo $cmd
 $cmd
-
-fileKmer="${fnamebase}.solid_kmers_binary";
-filePacked="${fnamebase}.solid_kmers_binary.packed";
-
-cmd="$CosmoPack $fileKmer"
-echo $cmd
-$cmd
-
-# cmd="cat $fileFasta $fileFasta2" 
-# echo $cmd 
-# $cmd > merged.fasta
-
-# cmd="$dsk merged.fasta $k"
-# echo $cmd
-# $cmd
-
-# fileKmer2="merged.solid_kmers_binary";
-# filePacked2="merged.solid_kmers_binary.packed";
-
-# cmd="$CosmoPack $fileKmer2"
-# echo $cmd
-# $cmd
-
-cmd="$cosmodel $filePacked $fileFasta2"
-echo $cmd
-$cmd
-
-
-
